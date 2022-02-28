1289d4438f5bafb87f3ba0e609b6e4761b1ffc6c
S-T-A-L-K-R <name.generate@gmail.com>
2022-02-28 19:22:30 +0100

    4/4 test passed

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TailsGenerator {
-
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        List<String> tail = new ArrayList<>(value.length() + 1);
+        for(int i = 0; i < value.length() + 1; i++)
+        {
+            tail.add(value.substring(i));
+        }
+        return tail;
     }
 
 }
